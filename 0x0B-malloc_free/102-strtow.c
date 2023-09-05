468841a2fbfcbf01e024222e4547816fa383b146
AlexanderSahle <alexandersahle@gmail.com>
2023-09-05 04:44:03 +0300

    Malloc is like your seat/reservation

---
+++ 0x0B-malloc_free/102-strtow.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * words are separeted by space
+ *
+ * str: apointed to an an array
+ *
+ * Return: (words)
+ */
+
+char **strtow(char *str)
+{
+	char str;
