ee29c346a5ed0ce1ea0dedf311cfc59ddb4ca214
AlexanderSahle <alexandersahle@gmail.com>
2023-09-05 04:55:29 +0300

    Static_lib

---
+++ 0x09-static_libraries/alx-low_level_programming/0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * main - main block
+ *
+ * Discription: Get a random it with 5 
+ * 
+ * Retern: Always 0(Success)
+*/
+
+int main(void)
+{
+        char c = 'a';
+
+	while (c <= 'z')
+        {
+		putchar(c);
+		c++;
+	}
+	putchar('\n')
+        return (0);
+}
