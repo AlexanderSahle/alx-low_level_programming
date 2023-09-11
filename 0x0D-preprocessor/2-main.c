2934eaf18c039155298f881f9615b745f5cf3c53
AlexanderSahle <alexandersahle@gmail.com>
2023-09-12 02:53:23 +0300

    SUM

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - entry point
+ *
+ * Description: prints the name of the file it was compiled from,
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+
+	return (0);
+}
+
