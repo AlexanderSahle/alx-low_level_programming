ce872d3077122529fc9d687dc94a44f635508d3e
AlexanderSahele <alexandersahele@gmail.com>
2023-11-08 04:04:47 +0300

    FILE

---
+++ 0x0D-preprocessor/2-main.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+#ifndef _FILE_
+#define _FILE_
+
+#endif
+/**
+ * main - a function that call a file
+ *
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	printf("%s\n", _FILE_);
+	return (0);
+}
