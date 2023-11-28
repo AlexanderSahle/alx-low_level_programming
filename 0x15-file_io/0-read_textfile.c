869aa1c43b4179e6669ca1e235a5e655f25b3545
AlexanderSahele <alexandersahele@gmail.com>
2023-11-28 04:15:38 +0300

    update Tread lightly, she is near

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -17,8 +17,10 @@ ssize_t read_textfile(const char *filename, size_t letters)
 	ssize_t t;
 
 	fl = open(filename, O_RDONLY);
+
 	if (fl == -1)
 		return (0);
+
 	buffer = malloc(sizeof(char) * letters);
 	t = read(fl, buf, letters);
 	w = write(STDOUT_FILENO, buffer, t);
